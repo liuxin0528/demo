4a282634063adac73bd38ddb1a55af3009c2c3c0
liuxin <liuxin_bitcom@163.com>
2019-03-26 16:56:16 +0800

    接入druid

---
+++ src/main/java/com/example/demo/config/DruidConfiguration.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 by 青岛比特信息科技有限公司 .
+ * All rights reserved.
+ */
+package com.example.demo.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletRegistration;
+
+/**
+ * @author liuxin <br>
+ * @date 2019/3/26 16:39 <br>
+ * @project demo <br>
+ * @description <br>
+ */
+@Configuration
+public class DruidConfiguration {
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                new StatViewServlet(), "/druid/*"
+        );
+
+        servletRegistrationBean.addInitParameter("allow", "192.168.1.218, 127.0.0.1");
+        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
+
+        servletRegistrationBean.addInitParameter("loginUsername", "druid");
+        servletRegistrationBean.addInitParameter("loginPassword", "12345678");
+
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean startFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        filterRegistrationBean.addInitParameter("exclusions", "*.js, *.gif, *.jpg, *.png, *.css, *.ico, /druid/*");
+
+        return filterRegistrationBean;
+    }
+}
